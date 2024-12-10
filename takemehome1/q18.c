2517d2c69174129e312ab6ba10851cde75466db2
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2024-12-10 23:39:00 +0530

    Create q18.c

---
+++ takemehome1/q18.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define PRINT(i, limit) while(i<=limit)\
+{\
+printf("i=%d\n",i);\
+i=i+1;\
+}
+int main()
+{
+int i=0;
+PRINT(i, 6);
+return 0;
+}
+OUTPUT:
+i=0
+i=1
+i=2
+i=3
+i=4
+i=5
+i=6
