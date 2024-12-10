675347c10d26432900d0df7c7287dd05b4ae33af
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2024-12-10 23:53:14 +0530

    Create q16.c

---
+++ takemehome1/q16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define PRINT(int) printf("int=%d\n",int)
+int main(){
+int x=03,y=32,z=01;
+PRINT(xˆx);
+z<<=3;
+PRINT(z);
+y>>=3;
+PRINT(y);
+return 0;
+}
+OUTPUT:
+int=0
+int=8
+int=4
