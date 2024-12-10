b70e6a932557afaeb1e68dc432493213883a1129
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2024-12-10 23:19:19 +0530

    Create q5.c

---
+++ takemehome1/q5.c
@@ -0,0 +1,12 @@
+#define x 10+10
+#include <stdio.h>
+#define scanf "%s Practical C Y-N"
+int main(){
+int a=x*x;
+printf("%d\n",a);
+printf(scanf,scanf);
+return 0;
+}
+output:
+120
+%s Practical C Y-N %s Practical C Y-N
