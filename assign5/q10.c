198da15c8575be59a85c38ed446e6071c9ea379c
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2024-12-24 01:02:30 +0530

    Create q10.c

---
+++ assign5/q10.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int *p=NULL;
+p=(int *)malloc(sizeof(int));
+*p=10;
+free(p);
+int *q;
+q=(int *)malloc(sizeof(int));
+*q=15;
+printf("%d %d\n",*p,*q);
+return 0;}//output 15 15
