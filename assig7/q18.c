080472f36707b722d287e75d69b920d55c104a1d
Anindita Banerjee <165119244+Anindita1310@users.noreply.github.com>
2025-01-01 17:41:47 +0530

    Create q18.c

---
+++ assig7/q18.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+typedef struct{
+    char name[30];
+    double diameter;
+    int moons;
+    double or_time;
+    double ro_time
+}planet_t;
+int main(){
+    planet_t t1,*ptr;
+    ptr=&t1;
+    strcpy(ptr->name,"Anindita");
+    ptr->diameter=45.98;
+    ptr->moons=45;
+    ptr->or_time=23.56;
+    ptr->ro_time=45.67;
+    printf("%s %.2lf %d %.2lf %.2lf\n",ptr->name,ptr->diameter,ptr->moons,ptr->or_time,ptr->ro_time);
+}
